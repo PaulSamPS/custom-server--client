b94a2df337fc96b4d9623844cbdbc2833e3fe0bf
Pavel Samoylenko <paulsamv@yandex.ru>
2024-07-18 13:46:31 +0300

    .efe.

--- App.js
+++ App.js
@@ -42,7 +42,7 @@ export default function App() {
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Updated!</Text>
+      <Text>Hello!</Text>
       <Text>{text}</Text>
       <ActivityIndicator />
     </View>
